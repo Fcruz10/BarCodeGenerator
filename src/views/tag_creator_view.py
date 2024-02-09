acfa29e2e5b4287475ef36ac8d9a3aa99af798c3
Francisco Joao Borges Mazeda Cruz <a54926@alunos.ipb.pt>
2024-02-09 02:32:44 +0000

    feat: Implementing Error Handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreateController
 
 class TagCreatorView:
     
@@ -8,10 +9,13 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreateController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # logic management rule
-        print("Im in View")
+        formatted_response = tag_creator_controller.create(product_code)
+        
         # return http
-        return HttpResponse(status_code = 200, body ={ "hello": "world" })
+        return HttpResponse(status_code = 200, body = formatted_response)
